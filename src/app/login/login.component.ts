044154e2fb1214ceccc8b350dcd51c6ed0b24cd7
Andy Siamas <andy@Andys-iMac-2.hsd1.ca.comcast.net>
2020-06-23 13:15:47 -0700

    Added properties for min and max username characters, injected router service, and created login method

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm, FormControl, NgModel } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,12 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('form', { static: false })
+  public form: NgForm;
+
+  public minUsernameChars: number = 4;
+  public maxUsernameChars: number = 20;
+
+  constructor(private router: Router) {}
 
   public ngOnInit(): void {}
 
   public login(): void {
-    console.log('LOGGING IN!');
+    if (this.form.valid) {
+      this.router.navigate(['browse']);
+    }
+  }
+
+  public getErrorMessage(username: NgModel): string {
+    if (username.value.length < this.minUsernameChars) {
+      return `Username cannot be shorter than ${this.minUsernameChars} characters.`;
+    } else {
+      return 'Invalid username.';
+    }
   }
 
 }
