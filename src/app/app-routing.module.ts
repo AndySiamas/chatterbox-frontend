8d240e57d3460790a4672252bf26c99fb6d8c6e6
Andy Siamas <andy@Andys-iMac-2.hsd1.ca.comcast.net>
2020-06-23 14:08:59 -0700

    Added chatroom and id to app routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { BrowseComponent } from './browse/browse.component';
+import { ChatroomComponent } from './chatroom/chatroom.component';
 
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'browse', component: BrowseComponent },
+  { path: 'rooms/:id', component: ChatroomComponent },
   { path: '**', redirectTo: '/login', pathMatch: 'full' }
 ];
 
