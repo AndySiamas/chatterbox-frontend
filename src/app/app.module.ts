78fd01328f91ca1f796c3297add006ec91782af1
Andy Siamas <andy@Andys-iMac-2.hsd1.ca.comcast.net>
2020-06-23 14:09:27 -0700

    Declared ChatroomComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { BrowseComponent } from './browse/browse.component';
+import { ChatroomComponent } from './chatroom/chatroom.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    BrowseComponent
+    BrowseComponent,
+    ChatroomComponent
   ],
   imports: [
     BrowserModule,
