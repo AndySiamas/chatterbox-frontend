a2674fceb10fe720dab2e50180eae060fa19f956
Andy Siamas <andy@Andys-iMac-2-155.hsd1.ca.comcast.net>
2020-06-25 13:37:37 -0700

    Created module to handle socketio configuration

---
+++ src/app/socketio-config/socketio-config.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+import { environment } from '../../environments/environment';
+
+const config: SocketIoConfig = { url: environment.socketEndpoint, options: {} };
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SocketIoModule.forRoot(config)
+  ],
+  exports: [SocketIoModule]
+})
+export class SocketIoConfigurationModule {}
