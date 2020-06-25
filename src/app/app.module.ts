7d0aef14caf8d3f03a54bf808e29ee0879d058a3
Andy Siamas <andy@Andys-iMac-2-155.hsd1.ca.comcast.net>
2020-06-25 13:39:28 -0700

    Added SocketIoConfigurationModule to imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { SocketIoConfigurationModule } from './socketio-config/socketio-config.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,7 +19,8 @@ import { ChatroomComponent } from './chatroom/chatroom.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    SocketIoConfigurationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
