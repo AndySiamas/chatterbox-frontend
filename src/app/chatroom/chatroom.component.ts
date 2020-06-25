2d8b31e84ed29dc1950ad624eb97ce37e94814d4
Andy Siamas <andy@Andys-iMac-2-155.hsd1.ca.comcast.net>
2020-06-25 13:47:06 -0700

    Chatroom component subscribes to roomId on init

--- src/app/chatroom/chatroom.component.ts
+++ src/app/chatroom/chatroom.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
+import User from '../shared/models/user.model';
+import Message from '../shared/models/message.model';
+import { ChatroomService } from './chatroom.service';
 
 @Component({
   selector: 'app-chatroom',
@@ -7,9 +12,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatroomComponent implements OnInit {
 
-  public constructor() {
+  public id: string;
+  private paramsSub: Subscription;
+  public users: User[] = [];
+  public messages: Message[] = [];
+
+  @ViewChild('userinput')
+  public userInput: ElementRef;
+
+  public constructor(private route: ActivatedRoute,
+                     private chatService: ChatroomService) {}
+
+  public ngOnInit(): void {
+    this.subscribeToParams();
   }
 
-  public ngOnInit(): void {}
+  private subscribeToParams(): void {
+    this.paramsSub = this.route.params.subscribe((params: Params) => {
+      this.id = params['id'];
+    });
+  }
+
+  public onSendMessage(): void {
+    console.log(this.userInput.nativeElement.value);
+  }
 
 }
