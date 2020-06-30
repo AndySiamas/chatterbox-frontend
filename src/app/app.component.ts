f3449536d4c1da2adeaba5bb394433538c7a8e9d
Andy Siamas <andy@Andys-iMac-2.hsd1.ca.comcast.net>
2020-06-30 14:40:06 -0700

    Injected auth service into app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './auth/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'chatterbox';
+export class AppComponent implements OnInit {
+
+  public title: string = 'ChatterBox';
+
+  public constructor(private authService: AuthService) {}
+
+  public ngOnInit(): void {}
 }
