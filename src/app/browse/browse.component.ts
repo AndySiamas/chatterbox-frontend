5a9fca146023cfe592903d0bfbad77a2e8386611
Andy Siamas <andy@Andys-iMac-2.hsd1.ca.comcast.net>
2020-06-30 14:47:56 -0700

    Browse component now sets default chatrooms based on the ones given to it by the server

--- src/app/browse/browse.component.ts
+++ src/app/browse/browse.component.ts
@@ -1,24 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import Chatroom from '../shared/models/chatroom.model';
 import { Router } from '@angular/router';
+import { NetworkService } from '../network/network.service';
+import { Socket } from 'ngx-socket-io';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-browse',
   templateUrl: './browse.component.html',
   styleUrls: ['./browse.component.scss']
 })
-export class BrowseComponent implements OnInit {
+export class BrowseComponent implements OnInit, OnDestroy {
 
-  public chatrooms: Chatroom[] = [ { name: 'General', id: 'general' }, 
-                                   { name: 'Health', id: 'health' }, 
-                                   { name: 'Beauty', id: 'beauty' }, 
-                                   { name: 'Sports', id: 'sports' },
-                                   { name: 'Technology', id: 'technology' },
-                                   { name: 'Dating', id: 'dating' } ];
+  public chatrooms: Chatroom[] = [];
 
-  public constructor(private router: Router) {}
+  public constructor(private router: Router,
+                     private networkService: NetworkService,
+                     private socket: Socket) {}
 
-  public ngOnInit(): void {}
+  public ngOnInit(): void {
+    this.setDefaultChatrooms();
+  }
+
+  public ngOnDestroy(): void {
+    this.setDefaultChatrooms();
+  }
+
+  private async setDefaultChatrooms(): Promise<void> {
+    this.chatrooms = await this.networkService.getDefaultChatrooms().toPromise();
+  }
 
   public onChatroomClick(chatroom: Chatroom): void {
     this.router.navigate(['rooms', chatroom.id]);
