4e6ff641945c9b83d46e724e4fa1b5e11c296f88
Andy Siamas <andy@Andys-iMac-2.hsd1.ca.comcast.net>
2020-06-30 14:42:23 -0700

    Modified auth guard to check if user has a valid token and redirects to login path if not

--- src/app/auth/auth-guard.service.ts
+++ src/app/auth/auth-guard.service.ts
@@ -11,16 +11,15 @@ export class AuthGuardService implements CanActivate {
   public constructor(private authService: AuthService,
                      private router: Router) {}
 
-  public canActivate(route: ActivatedRouteSnapshot, 
-                     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    return this.authService.isAuthenticated()
-                           .then((isAuthenticated: boolean) => {
-                              if (isAuthenticated) {
-                                return true;
-                              } else {
-                                this.router.navigate(['/login']);
-                                return false;
-                              }
-                           });
+  public async canActivate(route: ActivatedRouteSnapshot, 
+                           state: RouterStateSnapshot): Promise<boolean> {
+    let isAuthenticated: boolean = await this.authService.isAuthenticated();
+    if (isAuthenticated) {
+      this.authService.emitAuthorizedEvent();
+      return true;
+    }
+
+    this.router.navigate(['/login']);
+    return false;
   }
 }
