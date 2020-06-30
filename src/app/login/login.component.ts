2c3654a2ee4385ec81b1c38915b20525d79518dc
Andy Siamas <andy@Andys-iMac-2.hsd1.ca.comcast.net>
2020-06-30 14:55:05 -0700

    Login component login function now validates user token with the server first

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm, FormControl, NgModel } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -15,14 +16,18 @@ export class LoginComponent implements OnInit {
   public minUsernameChars: number = 4;
   public maxUsernameChars: number = 20;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router,
+              private authService: AuthService) {}
 
   public ngOnInit(): void {}
 
-  public login(): void {
-    if (this.form.valid) {
-      this.router.navigate(['browse']);
+  public async login(username: NgModel): Promise<void> {
+    if (this.form.invalid) return;
+    this.authService.setNewUsername(username.value);
+    if (!this.authService.hasUserToken()) {
+      await this.authService.setUserTokenFromServer();
     }
+    this.router.navigate(['browse']);
   }
 
   public getErrorMessage(username: NgModel): string {
