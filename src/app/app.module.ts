b8df543fbed13072699cf9a0954f295051d1120d
Andy Siamas <andy@Andys-iMac-2.hsd1.ca.comcast.net>
2020-06-30 14:40:41 -0700

    Added HttpClientModule to app imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { SocketIoConfigurationModule } from './socketio-config/socketio-config.module';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -20,6 +21,7 @@ import { ChatroomComponent } from './chatroom/chatroom.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    HttpClientModule,
     SocketIoConfigurationModule
   ],
   providers: [],
