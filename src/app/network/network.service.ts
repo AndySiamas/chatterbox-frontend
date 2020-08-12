76ce8fda9dc2a1eb1a011268705653fbea26a693
Andy Siamas <andy@Andys-iMac-2-180.hsd1.ca.comcast.net>
2020-08-12 12:09:26 -0700

    Updated network service to conform to new server API

--- src/app/network/network.service.ts
+++ src/app/network/network.service.ts
@@ -5,6 +5,7 @@ import { Token } from '../shared/models/token.model';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { environment as env } from 'src/environments/environment';
 import Chatroom from '../shared/models/chatroom.model';
+import ChatterboxResponse from '../../../../project-shared/models/chatterbox-response.model';
 
 @Injectable({
   providedIn: 'root'
@@ -16,6 +17,21 @@ export class NetworkService {
   constructor(private socket: Socket,
               private http: HttpClient) {}
 
+  public requestLogin(username: string, password: string): Observable<ChatterboxResponse> {
+    return this.http.post<ChatterboxResponse>(env.mainServerHost + env.loginEndpoint, {
+      username: username,
+      password: password
+    });
+  }
+
+  public requestSignup(username: string, password: string, email: string): Observable<ChatterboxResponse> {
+    return this.http.post<ChatterboxResponse>(env.mainServerHost + env.signupEndpoint, {
+      username: username,
+      email: email,
+      password: password
+    });
+  }
+
   public requestUserToken(username: string): Observable<Token> {
     let searchParams = new HttpParams();
     searchParams = searchParams.append('username', username);
@@ -24,10 +40,11 @@ export class NetworkService {
     });
   }
 
-  public requestIfTokenIsValid(userToken: string): Observable<boolean> {
+  public requestIfTokenIsValid(username: string, token: string): Observable<ChatterboxResponse> {
     let searchParams = new HttpParams();
-    searchParams = searchParams.append('token', userToken);
-    return this.http.get<boolean>(env.mainServerHost + env.requestValidationForTokenEndpoint, {
+    searchParams = searchParams.append('username', username);
+    searchParams = searchParams.append('token', token);
+    return this.http.get<ChatterboxResponse>(env.mainServerHost + env.requestValidationForTokenEndpoint, {
       params: searchParams
     });
   }
