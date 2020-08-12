64155f81db1db7277ce3d67e43255a0d113a8dde
Andy Siamas <andy@Andys-iMac-2-180.hsd1.ca.comcast.net>
2020-08-12 12:01:25 -0700

    Login and signup paths now include data that describes which type of form to display

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { BrowseComponent } from './browse/browse.component';
 import { ChatroomComponent } from './chatroom/chatroom.component';
-import { AuthGuardService } from './auth/auth-guard.service';
+import { AuthGuardService } from './auth/auth.guard';
 import { RoomGuardService } from './auth/room-guard.service';
 
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent },
+  { path: 'login', component: LoginComponent, data: { formType: 'login' } },
+  { path: 'signup', component: LoginComponent, data: { formType: 'signup' } },
   { path: 'browse', component: BrowseComponent, canActivate: [AuthGuardService] },
   { path: 'rooms/:id', component: ChatroomComponent, canActivate: [AuthGuardService, RoomGuardService] },
   { path: '**', redirectTo: '/login', pathMatch: 'full' }
